968eebae362f2744f03fcb24f467716533b26c6f
klxcoder <klxcoder@gmail.com>
2025-03-22 08:23:03 +0700

    Vector Search with Metadata Filtering

--- index.py
+++ index.py
@@ -29,3 +29,42 @@ print("Vector dim:", len(data[0]["vector"]))
 res = client.insert(collection_name="demo_collection", data=data)
 
 print(res)
+
+# query_vectors = embedding_fn.encode_queries(["Who is Alan Turing?"])
+# If you don't have the embedding function you can use a fake vector to finish the demo:
+query_vectors = [ [ random.uniform(-1, 1) for _ in range(768) ] ]
+
+res = client.search(
+    collection_name="demo_collection",  # target collection
+    data=query_vectors,  # query vectors
+    limit=2,  # number of returned entities
+    output_fields=["text", "subject"],  # specifies fields to be returned
+)
+
+print(res)
+
+# Insert more docs in another subject.
+
+docs = [
+    "Machine learning has been used for drug design.",
+    "Computational synthesis with AI algorithms predicts molecular properties.",
+    "DDR1 is involved in cancers and fibrosis.",
+]
+vectors = [[random.uniform(-1, 1) for _ in range(768)] for _ in docs]
+data = [
+    {"id": 3 + i, "vector": vectors[i], "text": docs[i], "subject": "biology"}
+    for i in range(len(vectors))
+]
+
+client.insert(collection_name="demo_collection", data=data)
+
+res = client.search(
+    collection_name="demo_collection",
+    # data=embedding_fn.encode_queries(["tell me AI related information"]),
+    data=[ [ random.uniform(-1, 1) for _ in range(768) ] ],
+    filter="subject == 'biology'",
+    limit=2,
+    output_fields=["text", "subject"],
+)
+
+print(res)
