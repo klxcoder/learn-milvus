177f46288055b0b9b408ed01133f8a0991ee9799
klxcoder <klxcoder@gmail.com>
2025-03-22 08:14:23 +0700

    vectors

--- index.py
+++ index.py
@@ -9,3 +9,19 @@ client.create_collection(
     collection_name="demo_collection",
     dimension=768,  # The vectors we will use in this demo has 768 dimensions
 )
+
+import random
+
+docs = [
+    "Artificial intelligence was founded as an academic discipline in 1956.",
+    "Alan Turing was the first person to conduct substantial research in AI.",
+    "Born in Maida Vale, London, Turing was raised in southern England.",
+]
+vectors = [[random.uniform(-1, 1) for _ in range(768)] for _ in docs]
+data = [
+    {"id": i, "vector": vectors[i], "text": docs[i], "subject": "history"}
+    for i in range(len(vectors))
+]
+
+print("Data has", len(data), "entities, each with fields: ", data[0].keys())
+print("Vector dim:", len(data[0]["vector"]))
